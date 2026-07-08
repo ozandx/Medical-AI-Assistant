f6c042e84619342dea8f570341379b7a5ce9b342
FauzanD <144305034+ozandx@users.noreply.github.com>
2026-07-08 08:14:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ from google.generativeai.types import HarmCategory, HarmBlockThreshold
 # CONFIGURATION
 # =====================================================
 
-load_dotenv()
-
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 generation_config = {
     "temperature": 0.3,
